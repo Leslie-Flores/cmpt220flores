20da868afaaf8f24fd1f49e7ecdaa7768dafc727
Leslie Flores <leslie.flores1@marist.edu>
2017-03-30 23:46:04 -0400

    Adding updated version.

--- prj/1/Driver_prj1.java
+++ prj/1/Driver_prj1.java
@@ -3,30 +3,41 @@
 // Course: CMPT220
 // Assignment: Making a Convolution (Project 1)
 // Due date: March 30th, 2017
-// Version 1.0
+// Version 1.1
+
+// DON'T FORGET TO INCLUDE MEANINGFUL COMMENTS
 
 import java.util.Scanner;
 
 public class Driver_prj1 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-		int[] vFirst = new int[input.nextInt()];
-    int[] vSecond = new int[input.nextInt()];
-    System.out.println();
-    for (int i = 0; i < vFirst.length; i++) {
-			vFirst[i] = input.nextInt();
-    }
-    for (int i = 0; i < vSecond.length; i++) {
-			vSecond[i] = input.nextInt();
-    }
+    int svf = input.nextInt();
+    int svs = input.nextInt();
+
+    double[] vFirst = new double[svf];
+    double[] vSecond = new double[svs];
+
+    for (int i = 0; i < svf; i++)
+      vFirst[i] = input.nextDouble();
+    
+    for (int i = 0; i < svs; i++)
+      vSecond[i] = input.nextDouble();
+
+    double[] vector = convolveVectors(vFirst, vSecond);
+    
+    for (int i = 0; i < vector.length; i++)
+      System.out.print(Math.round(vector[i]) + " ");
   }
 
   public static int[] convolveVectors(int[] vFirst, int[] vSecond) {
     int[] vResult = new int[(vFirst.length + vSecond.length) - 1];
-    for (int i = 0; i < vFirst.length; i--){
-      System.out.print(vFirst.length); 
+    for (int idx = 0; idx < vResult.length; idx++){
+      for (int shift = 0; shift < vSecond.length; shift++){
+        if (((idx - shift) >= 0) && ((idx - shift) < vFirst.length))
+         vResult[idx] += vFirst[idx - shift] * vSecond[shift];
+      }
     }
     return vResult;  
   }
-
 }
