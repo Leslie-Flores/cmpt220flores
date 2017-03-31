b01dd5055e6a6f82a3ed36047d40e16139132c47
Leslie Flores <leslie.flores1@marist.edu>
2017-03-30 23:52:13 -0400

    Adding the required comments for project 1.

--- prj/1/Driver_prj1.java
+++ prj/1/Driver_prj1.java
@@ -3,41 +3,39 @@
 // Course: CMPT220
 // Assignment: Making a Convolution (Project 1)
 // Due date: March 30th, 2017
-// Version 1.1
-
-// DON'T FORGET TO INCLUDE MEANINGFUL COMMENTS
+// Version 1.3
 
 import java.util.Scanner;
 
 public class Driver_prj1 {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    int svf = input.nextInt();
+    Scanner input = new Scanner(System.in); 
+    int svf = input.nextInt(); // here it is asking for the lengths of input
     int svs = input.nextInt();
 
-    double[] vFirst = new double[svf];
+    double[] vFirst = new double[svf]; // here it is asking for the inputs
     double[] vSecond = new double[svs];
 
-    for (int i = 0; i < svf; i++)
+    for (int i = 0; i < svf; i++) // checking for the length of vFirst
       vFirst[i] = input.nextDouble();
     
-    for (int i = 0; i < svs; i++)
+    for (int i = 0; i < svs; i++) // checking for the length of vSecond
       vSecond[i] = input.nextDouble();
 
-    double[] vector = convolveVectors(vFirst, vSecond);
+    double[] vector = convolveVectors(vFirst, vSecond); // Where a double/int conversion issue occurs
     
-    for (int i = 0; i < vector.length; i++)
+    for (int i = 0; i < vector.length; i++) // Doesn't work due to issue lised above
       System.out.print(Math.round(vector[i]) + " ");
   }
 
   public static int[] convolveVectors(int[] vFirst, int[] vSecond) {
     int[] vResult = new int[(vFirst.length + vSecond.length) - 1];
-    for (int idx = 0; idx < vResult.length; idx++){
-      for (int shift = 0; shift < vSecond.length; shift++){
-        if (((idx - shift) >= 0) && ((idx - shift) < vFirst.length))
-         vResult[idx] += vFirst[idx - shift] * vSecond[shift];
+    for (int idx = 0; idx < vResult.length; idx++){ // checks to see if index is less than vResult length
+      for (int shift = 0; shift < vSecond.length; shift++){ // checks to see if the shift goes through all values of vSecond
+        if (((idx - shift) >= 0) && ((idx - shift) < vFirst.length)) // functions is here to avoid out of bounds issues
+         vResult[idx] += vFirst[idx - shift] * vSecond[shift]; // this is where the list shifts to convolve
       }
     }
-    return vResult;  
+    return vResult; // sends the results to be printed
   }
 }
