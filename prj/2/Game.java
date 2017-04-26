e46139016ecf49e6d67bef46ee40eb11a2c85e4b
Leslie Flores <leslie.flores1@marist.edu>
2017-04-25 23:16:37 -0400

    Adding V 0.2 with better updates.

--- prj/2/Game.java
+++ prj/2/Game.java
@@ -5,42 +5,68 @@
 import java.util.Scanner;
 
 public class Game{
+  private static Scanner scanner = new Scanner(System.in);
 	public static void main (String[] args){
     Scanner input = new Scanner(System.in);
     String playerLoc;
     int playerScore;
     int score = 5;
-    String intro = "\n Welcome to Life of a Syrian Refugee: The Journey."
-                 + "\n This is version 0.1 of this game so there will be updates.";
-    String home = "\n Hasan Ali wakes up to the sound of loud bombing nearby."
-                + "\n His wife, Aisha, and their two children are now awake, huddling."
-                + "\n He reassures them that they'll be okay when the house shakes."
-                + "\n After the shaking stops, the Ali family realizes their home was destroyed."
-                + "\n Now, with no home, the family has to leave or risk dying from the dangers.";
-    String hometown = "\n After a restless night, Hasan packs their remaining belongings."
-                    + "\n Aisha made sure to have their children ready to go and looks at her husband."
-                    + "\n Hasan is worried about the troubles they'll encounter and their safety."
-                    + "\n After realizing that most of their neighbors are dead, they set off carefully."
-                    + "\n Hasan takes one look back at his destroyed hometown before departing.";
-    String checkpoint = "\n After a couple hours, the family grows tired of walking so much."
-                      + "\n Hasan decides to hold his youngest, little Ahmed, so he can rest."
-                      + "\n The child soon points out that there's something up ahead."
-                      + "\n Hasan's heart sinks when he realizes that it's a military checkpoint."
-                      + "\n Taking another route bypassing the check, the family avoids trouble.";
-    String village = "\n Welcome to the Al-Hwash village.";
-    String sea = "\n Welcome to the Mediterranean Sea.";
-    String boat = "\n On your way to freedom for your family.";
-    String land = "\n You made it to Turkey! Congrats! You won!.";
+    String intro = "Welcome to Life of a Syrian Refugee: The Journey. \n"
+                 + "This is version 0.2 of this game so there will be updates. \n";
+                 
+    System.out.println(intro);
+    System.out.print("What is your name? ");
+    String name = scanner.nextLine();
+    System.out.println();
     boolean isAlive = true;
     
-    
-		System.out.println(intro);
+    String home = name + " Ali wakes up to the sound of loud bombing nearby. \n"
+                + "His wife, Aisha, and their two children are now awake, huddling. \n"
+                + "He reassures them that they'll be okay when the house shakes. \n"
+                + "After the shaking stops, the Ali family realizes their home was destroyed. \n"
+                + "Now, with no home, the family has to leave or risk dying from the dangers. \n";
+    String pressEnter = "\n Please press Enter to continue. ";
+    String hometown = "After a restless night, " + name + " packs their remaining belongings. \n"
+                    + "Aisha made sure to have their children ready to go and looks at her husband. \n"
+                    + name + " is worried about the troubles they'll encounter and their safety. \n"
+                    + "After realizing that most of their neighbors are dead, they set off carefully. \n"
+                    + name + " takes one look back at his destroyed hometown before departing. \n";
+    String checkpoint = "After a couple hours, the family grows tired of walking so much. \n"
+                      + name + " decides to hold his youngest, little Ahmed, so he can rest. \n"
+                      + "The child soon points out that there's something up ahead. \n"
+                      + name + "'s heart sinks when he realizes that it's a military checkpoint. \n"
+                      + "Taking another route bypassing the check, the family avoids trouble. \n";
+    String village = "After a few days of heading westward to Turkey, the family is really tired. \n"
+                   + "Aisha is a couple months pregnant and is clearly really tired of this walk. \n"
+                   + "They soon come across a village, which is clearly Christian, making Hasan worried. \n"
+                   + "However, the family finds that the villagers are welcoming and lets them rest. \n"
+                   + "After a goodnight's sleep, the family bids them farewell and sets off again. \n";
+    String sea = "Now more than halfway there, the family soon comes across a port city in view. \n"
+               + "There are other families there, eager and anxious to leave this place. \n"
+               + name + "'s eldest, Daniyah, clings to her father and asks when they will go. \n"
+               + "After much bargaining with the smuggler, " + name + " manages to get them on the last boat. \n"
+               + "As they leave, he takes one last look at his country that he has known his entire life. \n";
+    String boat = "The boat is rather poorly built and clearly has not been maintained in recent years. \n"
+                + "However, after much praying, the boat still remains intact despite it's shape. \n"
+                + "There's a storm nearby and it makes the waters very choppy and everyone seasick. \n"
+                + name + "'s family stick together and are not thrown off the boat by the storm. \n"
+                + "Soon enough, the storm clears and land is seen in the horizon much to their relief. \n";
+    String land = "The land that everyone saw turns out to be Turkey and they are not that far to it. \n"
+                + "Everyone celebrates and " + name + " sighs in relief that his family will be safe. \n"
+                + "They encounter the Turkish coastal guard who sees them and takes them to shore. \n"
+                + name + "'s nervous that they'll send everyone back to Turkey as he's seen before. \n"
+                + "But that doesn't happen as they take everyone to a refugee camp. \n";
+    String youWon = "Congrats! You have made it to Turkey! Good job and thanks for playing! \n";
+    String copyright = "Copyright: 2017, Leslie Flores, leslie.flores1@marist.edu";
   
     System.out.print(home);
     playerScore = 0;
     playerLoc = "Destroyed Home";
     System.out.print("\n Score: " + playerScore);
     System.out.println("\n Location: " + playerLoc);
+    System.out.print(pressEnter);
+    String enter = scanner.nextLine();
+    System.out.println();
     isAlive = true;
 
     System.out.print(hometown);
@@ -48,6 +74,9 @@ public class Game{
     playerLoc = "Destroyed Hometown";
     System.out.print("\n Score: " + playerScore);
     System.out.println("\n Location: " + playerLoc);
+    System.out.print(pressEnter);
+    String enter2 = scanner.nextLine();
+    System.out.println();
     isAlive = true;
     
     System.out.print(checkpoint);
@@ -55,6 +84,9 @@ public class Game{
     playerLoc = "Checkpoint";
     System.out.print("\n Score: " + playerScore);
     System.out.println("\n Location: " + playerLoc);
+    System.out.print(pressEnter);
+    String enter3 = scanner.nextLine();
+    System.out.println();
     isAlive = true;
     
     System.out.print(village);
@@ -62,6 +94,9 @@ public class Game{
     playerLoc = "Village";
     System.out.print("\n Score: " + playerScore);
     System.out.println("\n Location: " + playerLoc);
+    System.out.print(pressEnter);
+    String enter4 = scanner.nextLine();
+    System.out.println();
     isAlive = true;
     
     System.out.print(sea);
@@ -69,6 +104,9 @@ public class Game{
     playerLoc = "Mediterranean Sea";
     System.out.print("\n Score: " + playerScore);
     System.out.println("\n Location: " + playerLoc);
+    System.out.print(pressEnter);
+    String enter5 = scanner.nextLine();
+    System.out.println();
     isAlive = true;
     
     System.out.print(boat);
@@ -76,6 +114,9 @@ public class Game{
     playerLoc = "Dinghy";
     System.out.print("\n Score: " + playerScore);
     System.out.println("\n Location: " + playerLoc);
+    System.out.print(pressEnter);
+    String enter6 = scanner.nextLine();
+    System.out.println();
     isAlive = true;
     
     System.out.print(land);
@@ -83,6 +124,10 @@ public class Game{
     playerLoc = "Turkey";
     System.out.print("\n Final Score: " + playerScore);
     System.out.println("\n Final Location: " + playerLoc);
+    System.out.println();
+    System.out.print(youWon);
     isAlive = true;
+    
+    System.out.println(copyright);
 	}
 }
